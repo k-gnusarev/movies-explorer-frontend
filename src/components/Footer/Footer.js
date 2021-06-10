368ac530468cad2df1d868f3dcd759a50290e115
Konstantin Gnusarev <kgnusarev@griddynamics.com>
2021-06-10 23:48:12 +0400

    add: footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,30 @@ import './Footer.css';
 
 export default function Footer(props) {
   return (
-    <footer>
-      <p>here be footer</p>
+    <footer className='footer'>
+      <p className='footer__text'>Учебный проект Яндекс.Практикум х BeatFilm</p>
+      <div className='footer__wrapper'>
+        <p className='footer__copyright'>© 2020</p>
+        <nav className='footer__nav'>
+          <ul className='footer__list'>
+            <li className='footer__item'>
+              <a href='#' target='_blank' rel='noreferrer' className='footer__link'>
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li className='footer__item'>
+              <a href='#' target='_blank' rel='noreferrer' className='footer__link'>
+                GitHub
+              </a>
+            </li>
+            <li className='footer__item'>
+              <a href='#' target='_blank' rel='noreferrer' className='footer__link'>
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </footer>
   )
 };
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './Main.css';
 import Promo from '../Promo/Promo';
-import NavTab from '../NavTab/NavTab';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
 import AboutMe from '../AboutMe/AboutMe';
+import Portfolio from '../Portfolio/Portfolio';
 
 export default function Main(props) {
   return (
@@ -13,6 +13,7 @@ export default function Main(props) {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Portfolio />
     </main>
   )
 };
