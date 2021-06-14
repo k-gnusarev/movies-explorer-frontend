886a7349ba66ab734f6c540878c380b3da411caf
Konstantin Gnusarev <kgnusarev@griddynamics.com>
2021-06-14 10:11:18 +0400

    add: 404 page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import './NotFound.css';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 export default function NotFound(props) {
+  const history = useHistory(); 
+  
   return (
-    <>
-    </>
+    <section className='not-found'>
+      <div className='not-found__container'>
+        <h2 className='not-found__title'>404</h2>
+        <p className='not-found__text'>Страница не найдена</p>
+        <p className='not-found__link' onClick={ () => history.goBack() }>Назад</p>
+      </div>
+    </section>
   )
 };
