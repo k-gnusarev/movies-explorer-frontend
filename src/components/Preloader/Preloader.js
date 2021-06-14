12e7d98b24d5b9a61a403cdb175d4672c1a6d04c
Konstantin Gnusarev <kgnusarev@griddynamics.com>
2021-06-14 14:31:46 +0400

    add: preloader

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -5,7 +5,9 @@ export default function Preloader(props) {
   
   return (
     <div className='preloader'>
-      
+      <div className='preloader__container'>
+          <span className='preloader__round'></span>
+      </div>
     </div>
   )
 };
